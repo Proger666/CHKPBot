ed6dac959a6f69137ead40d4b59502d3d67d0bb1
Proger666 <knorra@mail.ru>
2018-02-08 23:38:40 +0300

    initial

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0', ssl_context='adhoc')
+    app.run(debug=False, port=port, host='0.0.0.0')
